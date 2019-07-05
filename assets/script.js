0e432dd4a7635922d8db54d5950a0f3b51ffd0b5
Benjamin Friedrich <benjfriedrich@BFs-mac.local>
2019-07-04 21:49:48 -0500

    quick edits

--- assets/script.js
+++ assets/script.js
@@ -1,59 +1,22 @@
-
-
-
 function imgLoader() {
 	
 	var imgList = document.getElementsByClassName('loadMe');
 	var imgListLength = imgList.length;
 
-	console.log("imgList length = " + imgListLength);
-
 	load(0);
-
-	
-
-
-	// for (i = 0; i < imgListLength; i++ ) {
-		
-	// 	imgList[i].addEventListener('load', load(i+1))
-	
-	// }
-
-
-	// for (i = 0; i < imgListLength; i++) {
-
-	// 	if (i == 0 ) {
-
-	// 		imgList[i].src = imgList[i].dataset.imgsrc;
-
-	// 	};
-	// 	imgList[i]addEventListener
-
-		
-
-	// };
-
 
 	function load(i) {
 
-		console.log(i + " load is starting.");
 
 		imgList[i].src = imgList[i].dataset.imgsrc;
 		
 		if ( i + 1 < imgListLength ) {
 			
-			imgList[i].addEventListener('load', function() {console.log(i + " is loaded."); load(i+1);});
-
-		} else {
-
-			console.log(i + " is loaded. Loading complete.")
+			imgList[i].addEventListener('load', function() {load(i+1);});
 
 		};
-
 	}
 
 }
-
-
 
 document.addEventListener('load',imgLoader());
